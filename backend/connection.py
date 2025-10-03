917085b55d8b7f1c410375fcab2983f74d2894fb
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-03 18:16:51 +0530

    simplyfied k8s

--- backend/connection.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-from os import environ
-
-MONGO_URL = environ.get('MONGO_URL', 'mongodb://localhost:27017/gehenna')
-
-client = MongoClient(MONGO_URL)
-db = client['gehenna']
-coll = db['names']
